e515a6ced0edbebffdd7dedef6f5e94d2a29e21e
Dardin <logandalec@gmail.com>
2018-08-24 00:32:17 -0700

    fixed game logic, added console output

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,102 @@
-var game = {
-    start = true,
-    remain: 6,
-    guesses: [],
-    blanks: [],
-    wins: 0,
-    word: 'salmon',
-    // picks a random object from words list
-    wordPick: function () {
-        // pick random word/object, add appropriate number of blanks
-        // this.word = result;
-    },
+$(document).ready(function () {
+    var game = {
+        start: true,
+        remain: 6,
+        guesses: [],
+        blanks: [],
+        wins: 0,
+        word: 'salmon',
+        // picks a random object from words list
+        wordPick: function () {
+            // pick random word/object, 
+            // add appropriate number of blanks
+            for(var i = 0; i < this.word.length; i++) {
+                if (this.word[i] == " ") {
+                    this.blanks[i] = " ";
+                } else {
+                    this.blanks[i] = "_";
+                }
+            }
+            // this.word = result;
+        },
+        //updates html and game state to current guesses and blanks
+        update: function () {
+            // print to console first then figure out styling
+            console.log("blanks: " + this.blanks);
+            console.log("guesses: " + this.guesses);
+            console.log("remain: " + this.remain);
+            console.log("wins: " + this.wins)
+            
+            // var html = "<p>" + this.guesses + "</p>";
+            // $('#game').innerHTML(html)
+        },
+        // default display if someone wins, asks to play again.
+        win: function () {
+            console.log("you Win!")
+        },
+        // default display if someone loses asks to play again.
+        lose: function () {
+            console.log("You lose!")
+        },
+        //resets game state picks a new word.
+        reset: function () {
+            this.start = true;
+            this.remain = 6;
+            this.guesses = [];
+            this.blanks = [];
+            this.wordPick();
+        },
+        //finds indecies of letter in a given word and replaces same index in blanks
+        replaceBlnk: function (letter) {
+            for (var i = 0; i < this.word.length; i++) {
+                if (this.word[i] === letter) {
+                    this.blanks[i] = letter;
+                }
+            }
+        }
 
-    update: function () {
-        var html = "<p>" + this.guesses +"</p>";
-        $('#game').innerHTML(html)
-    },
-    // default display if someone wins, asks to play again.
-    win: function () {
+    };
 
-    },
-    // default display if someone loses asks to play again.
-    lose: function () {
-
-    },
-    reset: function () {
-
+    //checks if string is an a-z letter (assume lowercase)
+    function isLetter(str) {
+        return !!(str.length === 1 && str.match(/[a-z]/i));
     }
 
-};
+    //user guesses letter, runs through game logic
+    document.onkeyup = function (event) {
+        var guess = event.key.toLowerCase();
 
-
-document.onkeyup = fuction(event) {
-  var guess = event.key.toLowerCase();
-//   check if guess is letter.
-
-  //only pick word at match start
-  if (start){
-      game.wordPick();
-      game.start = false;
-  }
-  
-  // if out of Guesses -> end, else -> play game
-  if (game.remain === 0) {
-    game.lose()
-  } else { //play game
-    //   finds if guess is contained in word
-      if (game.word.search(guess) != -1){
-        //add letter to blanks
-        if (game.blanks.indexOf("_") == -1){
-            game.wins++;
-            game.win()
+        //only pick word at match start
+        if (game.start) {
+            game.wordPick();
+            game.start = false;
         }
-      } else if (game.guesses.indexOf(guess) != -1) {
-        game.remain--;
-        game.guesses.push(guess);
-        //update html/picture/add a womp womp sound
-      } else {
-          // you already guessed that!
-      }
-      
 
-
-  }
-  game.remain--;
-
-  game.guesses.push(event.key)
-  var html = "";
-  document.querySelector("#game").innerHTML = html;
-}
+        //check if guess is letter and continue
+        if (isLetter(guess)) {
+            // finds if guess is contained in word && not duplicate correct letter
+            if (game.word.includes(guess) && !game.blanks.includes(guess)) {
+                //replaces balnks with guess/updates game
+                game.replaceBlnk(guess);
+                game.update();
+                // wins if no blanks remaining
+                if (!game.blanks.includes("_")) {
+                    game.wins++;
+                    game.update();
+                    game.win();
+                }
+            // if guess is wrong but new
+            } else if (!game.guesses.includes(guess)) {
+                game.remain--;
+                if (game.remain === 0) {
+                    game.lose();
+                }
+                game.guesses.push(guess);
+                game.update();
+            //Guess is wrong and already guessed
+            } else {
+                //TODO: you already guessed that!
+            }
+        }
+    }
+});
